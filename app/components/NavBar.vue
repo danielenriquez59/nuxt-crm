f5e2d9e37bbf2b43c44c4c193f474fdfbef8721e
danielenriquez59 <deenriquez92@gmail.com>
2024-09-05 16:42:55 -0700

    initial commit

---
+++ app/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <UDashboardNavbar title="Home">
+        <template #right>
+          <UTooltip
+            text="Notifications"
+            :shortcuts="['N']"
+          >
+            <UButton
+              color="gray"
+              variant="ghost"
+              square
+              @click="isNotificationsSlideoverOpen = true"
+            >
+              <UChip
+                color="red"
+                inset
+              >
+                <UIcon
+                  name="i-heroicons-bell"
+                  class="w-5 h-5"
+                />
+              </UChip>
+            </UButton>
+          </UTooltip>
+
+          <UDropdown :items="items">
+            <UButton
+              icon="i-heroicons-plus"
+              size="md"
+              class="ml-1.5 rounded-full"
+            />
+          </UDropdown>
+        </template>
+      </UDashboardNavbar>
+</template>
+
+<script lang="ts" setup>
+const { isNotificationsSlideoverOpen } = useDashboard()
+const items = [[{
+  label: 'New mail',
+  icon: 'i-heroicons-paper-airplane',
+  to: '/inbox'
+}, {
+  label: 'New user',
+  icon: 'i-heroicons-user-plus',
+  to: '/users'
+}]]
+</script>
+
+<style>
+
+</style>
